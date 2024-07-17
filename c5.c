87b76aaaaa646a75b411b466e9c637c1c01d82a7
Devika-Soman <devikasoman32@gmail.com>
2024-07-17 12:11:44 +0530

    Add files via upload

---
+++ c5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,div;
+    printf("enter two numbers: ");
+    scanf("%d %d",&num1, &num2 );
+    div=num1/num2;
+     printf("div=%d\n",div);
+    return 0;
+}
