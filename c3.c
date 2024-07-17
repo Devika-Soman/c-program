bc7fe154e33beaad3f03841bc3725f9d73468b51
Devika-Soman <devikasoman32@gmail.com>
2024-07-17 12:07:53 +0530

    Add files via upload

---
+++ c3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,subb;
+    printf("enter two numbers: ");
+    scanf("%d %d",&num1, &num2 );
+    subb=num1-num2;
+     printf("subb=%d\n",subb);
+    return 0;
+}
